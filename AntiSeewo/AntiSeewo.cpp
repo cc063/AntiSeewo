e00e3c1ab73ea147a6bc1162a40891b9377d5e03
cc063 <cc063733@outlook.com>
2025-10-04 13:45:27 +0800

    Updated deafult config and uploaded a executable file

--- AntiSeewo/AntiSeewo.cpp
+++ AntiSeewo/AntiSeewo.cpp
@@ -403,7 +403,7 @@ int main() {
     "rtcRemoteDesktop.exe" };
     std::string defaultFileNameToCheck = "antiseewo.txt";
 
-    std::vector<std::string> defaultMonitorProcesses = { "screen_capture.exe", "rtcRemoteDesktop.exe", "notepad.exe" };
+    std::vector<std::string> defaultMonitorProcesses = { "screen_capture.exe", "rtcRemoteDesktop.exe" };
     std::vector<std::string> defaultTargetProcesses = { "explorer.exe" };
 
     std::vector<std::pair<std::string, std::string>> timeRanges = defaultTimeRanges;
